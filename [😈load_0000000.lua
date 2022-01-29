c307014e7df3a4f6d6e62e05f35af7d8d5e07e3f
kickTh <66652990+kickTh@users.noreply.github.com>
2022-01-29 13:19:46 +0700

    Add files via upload

---
+++ [😈load_0000000.lua
@@ -0,0 +1,599 @@
+gg.setVisible(false)
+gg.toast("▂                                         10% ")
+gg.toast("▂▃                                   20% ")
+gg.toast("▂▃▅                             30% ")
+gg.toast("▂▃▅▆                      40% ")
+gg.toast("▂▃▅▆██                   60% ")
+gg.toast("▂▃▅▆███                 65% ")
+gg.toast("▂▃▅▆████          80% ")
+gg.toast("▂▃▅▆█████     85% ")
+gg.toast("▂▃▅▆██████     90% ")
+gg.toast("▂▃▅▆███████100%")
+gg.toast("🛡️SCRIPT Car Parking 🛡️")
+function START()
+  vip = gg.choice({
+    "Money 🤑",
+    "Coin 💰 ",
+    " speed car",
+    "Car เติม ปลดล็อค All Free",
+    "⛔Exit⛔"
+  }, nil, " ━━━━━━━━━━━━━━━━━━━━━\n BY : MrHaHaX\n FB : Pbth Kick\n ➵ Car Parking\n━━━━━━━━━━━━━━━━━━━━━")
+  if vip == nil then
+  else
+  end
+  if vip == 1 then
+    AZ()
+  end
+  if vip == 2 then
+    BB()
+  end
+  if vip == 3 then
+    AZHG()
+  end
+  if vip == 4 then
+    DFHJHGKKKITSS()
+  end
+  if vip == 5 then
+    Exit()
+  end
+  GHOST = -1
+end
+
+function AZ()
+  AA = gg.choice({
+    "10M",
+    "20M",
+    "30M",
+    "40M",
+    "50M",
+    "ลบเงินทั้งหมด",
+    "🔙 กลับ ",
+    "⛔Exit⛔"
+  }, nil, "HACK MONEY 50,000,000")
+  if AA == nil then
+  else
+  end
+  if AA == 1 then
+    Hs()
+  end
+  if AA == 2 then
+    sd()
+  end
+  if AA == 3 then
+    hg()
+  end
+  if AA == 4 then
+    mm()
+  end
+  if AA == 5 then
+    kk()
+  end
+  if AA == 6 then
+    MAX()
+  end
+  if AA == 7 then
+    START()
+  end
+  if AA == 8 then
+    Exit()
+  end
+  GHOST = -1
+end
+
+function Hs()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.setVisible(false)
+  gg.searchNumber("60", gg.TYPE_FLOAT)
+  gg.getResults(200)
+  gg.editAll("340000", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.toast("10🇲")
+end
+
+function sd()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.setVisible(false)
+  gg.searchNumber("60", gg.TYPE_FLOAT)
+  gg.getResults(200)
+  gg.editAll("670000", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.toast("20🇲")
+end
+
+function hg()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.setVisible(false)
+  gg.searchNumber("60", gg.TYPE_FLOAT)
+  gg.getResults(200)
+  gg.editAll("1000000", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.toast("30🇲")
+end
+
+function mm()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.setVisible(false)
+  gg.searchNumber("60", gg.TYPE_FLOAT)
+  gg.getResults(200)
+  gg.editAll("1340000", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.toast("40🇲")
+end
+
+function kk()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.setVisible(false)
+  gg.searchNumber("60", gg.TYPE_FLOAT)
+  gg.getResults(200)
+  gg.editAll("1670000", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.toast("50🇲")
+end
+
+function MAX()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.setVisible(false)
+  gg.searchNumber("60", gg.TYPE_FLOAT)
+  gg.getResults(200)
+  gg.editAll("999999999", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.toast("ลบ สำเร็จ 🇲 🇦 🇽")
+end
+
+function BB()
+  DF = gg.choice({
+    " 100🄺 🄲🄾🄸🄽🅂",
+    " 200🄺 🄲🄾🄸🄽🅂",
+    " 300🄺 🄲🄾🄸🄽🅂",
+    " 400🄺 🄲🄾🄸🄽🅂",
+    " 500🄺 🄲🄾🄸🄽🅂",
+    "🔙 กลับ ",
+    "⛔Exit⛔"
+  }, nil, "HACK COINS 500,000 MAX")
+  if DF == nil then
+  else
+  end
+  if DF == 1 then
+    FD()
+  end
+  if DF == 2 then
+    FH()
+  end
+  if DF == 3 then
+    SY()
+  end
+  if DF == 4 then
+    BL()
+  end
+  if DF == 5 then
+    SG()
+  end
+  if DF == 6 then
+    START()
+  end
+  if DF == 7 then
+    Exit()
+  end
+  GHOST = -1
+end
+
+function FD()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.setVisible(false)
+  gg.alert("ɢᴏ ᴛᴏ ɴᴇᴡ ᴄᴀʀ ғɪɴᴅ ᴇᴋ")
+  gg.sleep(5000)
+  gg.alert(" ɴᴇxᴛ ᴛʜᴇ ʜᴏᴏᴅᴋɪᴛ ᴛᴏ 30ᴄᴏɪɴ")
+  gg.sleep(5000)
+  gg.searchNumber("30", gg.TYPE_DWORD)
+  gg.getResults(100)
+  gg.alert(" ɴᴇxᴛ ᴛʜᴇ ʜᴏᴏᴅᴋɪᴛ ɪɴᴛᴏ 50ᴄᴏɪɴ ")
+  gg.sleep(5000)
+  gg.refineNumber("50", gg.TYPE_DWORD)
+  gg.getResults(2)
+  gg.editAll("-100000", gg.TYPE_DWORD)
+  gg.clearResults(2)
+  gg.alert(" ɴᴇxᴛ ᴛʜᴇ ʜᴏᴏᴅ ᴋɪᴛ & ᴄʟɪᴄᴋ ɢᴇᴛ ʀᴇsᴛᴀʀᴛ ᴛʜᴇ ɢᴀᴍᴇ & sᴀᴠᴇ")
+  gg.toast("  𝐃 𝐎 𝐍 𝐄  ")
+end
+
+function FH()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.setVisible(false)
+  gg.alert("ɢᴏ ᴛᴏ ɴᴇᴡ ᴄᴀʀ ғɪɴᴅ ᴇᴋ")
+  gg.sleep(5000)
+  gg.alert(" ɴᴇxᴛ ᴛʜᴇ ʜᴏᴏᴅᴋɪᴛ ᴛᴏ 30ᴄᴏɪɴ")
+  gg.sleep(5000)
+  gg.searchNumber("30", gg.TYPE_DWORD)
+  gg.getResults(100)
+  gg.alert(" ɴᴇxᴛ ᴛʜᴇ ʜᴏᴏᴅᴋɪᴛ ɪɴᴛᴏ 50ᴄᴏɪɴ ")
+  gg.sleep(5000)
+  gg.refineNumber("50", gg.TYPE_DWORD)
+  gg.getResults(2)
+  gg.editAll("-200000", gg.TYPE_DWORD)
+  gg.clearResults(2)
+  gg.alert(" ɴᴇxᴛ ᴛʜᴇ ʜᴏᴏᴅᴋɪᴛ & ᴄʟɪᴄᴋ ɢᴇᴛ ʀᴇsᴛᴀʀᴛ ᴛʜᴇ ɢᴀᴍᴇ & sᴀᴠᴇ")
+end
+
+function SY()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.setVisible(false)
+  gg.alert("ɢᴏ ᴛᴏ ɴᴇᴡ ᴄᴀʀ ғɪɴᴅ ᴇᴋ")
+  gg.sleep(5000)
+  gg.alert(" ɴᴇxᴛ ᴛʜᴇ ʜᴏᴏᴅᴋɪᴛ ᴛᴏ 30ᴄᴏɪɴ")
+  gg.sleep(5000)
+  gg.searchNumber("30", gg.TYPE_DWORD)
+  gg.getResults(100)
+  gg.alert(" ɴᴇxᴛ ᴛʜᴇ ʜᴏᴏᴅ ᴋɪᴛ ɪɴᴛᴏ 50ᴄᴏɪɴ ")
+  gg.sleep(5000)
+  gg.refineNumber("50", gg.TYPE_DWORD)
+  gg.getResults(2)
+  gg.editAll("-300000", gg.TYPE_DWORD)
+  gg.clearResults(2)
+  gg.alert(" ɴᴇxᴛ ᴛʜᴇ ʜᴏᴏᴅᴋɪᴛ & ᴄʟɪᴄᴋ ɢᴇᴛ ʀᴇsᴛᴀʀᴛ ᴛʜᴇ ɢᴀᴍᴇ & sᴀᴠᴇ")
+end
+
+function BL()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.setVisible(false)
+  gg.alert("ɢᴏ ᴛᴏ ɴᴇᴡ ᴄᴀʀ ғɪɴᴅ ᴇᴋ")
+  gg.sleep(5000)
+  gg.alert(" ɴᴇxᴛ ᴛʜᴇ ʜᴏᴏᴅᴋɪᴛ ᴛᴏ 30ᴄᴏɪɴ")
+  gg.sleep(5000)
+  gg.searchNumber("30", gg.TYPE_DWORD)
+  gg.getResults(100)
+  gg.alert(" ɴᴇxᴛ ᴛʜᴇ ʜᴏᴏᴅ ᴋɪᴛ ɪɴᴛᴏ 50ᴄᴏɪɴ ")
+  gg.sleep(5000)
+  gg.refineNumber("50", gg.TYPE_DWORD)
+  gg.getResults(2)
+  gg.editAll("-4000000", gg.TYPE_DWORD)
+  gg.clearResults(2)
+  gg.alert(" ɴᴇxᴛ ᴛʜᴇ ʜᴏᴏᴅᴋɪᴛ & ᴄʟɪᴄᴋ ɢᴇᴛ ʀᴇsᴛᴀʀᴛ ᴛʜᴇ ɢᴀᴍᴇ & sᴀᴠᴇ")
+end
+
+function SG()
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.setVisible(false)
+  gg.alert("ɢᴏ ᴛᴏ ɴᴇᴡ ᴄᴀʀ ғɪɴᴅ ᴇᴋ")
+  gg.sleep(5000)
+  gg.alert(" ɴᴇxᴛ ᴛʜᴇ ʜᴏᴏᴅᴋɪᴛ ᴛᴏ 30ᴄᴏɪɴ")
+  gg.sleep(5000)
+  gg.searchNumber("30", gg.TYPE_DWORD)
+  gg.getResults(100)
+  gg.alert(" ɴᴇxᴛ ᴛʜᴇ ʜᴏᴏᴅ ᴋɪᴛ ɪɴᴛᴏ 50ᴄᴏɪɴ ")
+  gg.sleep(5000)
+  gg.refineNumber("50", gg.TYPE_DWORD)
+  gg.getResults(2)
+  gg.editAll("-500000", gg.TYPE_DWORD)
+  gg.clearResults(2)
+  gg.alert(" ɴᴇxᴛ ᴛʜᴇ ʜᴏᴏᴅᴋɪᴛ & ᴄʟɪᴄᴋ ɢᴇᴛ ʀᴇsᴛᴀʀᴛ ᴛʜᴇ ɢᴀᴍᴇ & sᴀᴠᴇ")
+end
+
+function AZHG()
+  menu = gg.choice({
+    "SPEED ON",
+    "SPEED OFF",
+    "🔙 กลับ ",
+    "⛔Exit⛔"
+  }, nil, "HACK SPEED CAR")
+  if menu == 1 then
+    AZHD()
+  end
+  if menu == 2 then
+    AZH()
+  end
+  if menu == 3 then
+    START()
+  end
+  if menu == 4 then
+    Exit()
+  end
+  GHOST = -1
+end
+
+function AZHD()
+  gg.clearResults()
+  io.output(fileData):write([[
+26429
+Var #7EAF125DC0|7eaf125dc0|10|3f80624e|0|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|24cadc0
+Var #7EC28A9380|7ec28a9380|4|c4653601|1|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|2735380
+Var #7EC28A9384|7ec28a9384|4|c4653601|1|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|2735384
+Var #7EC28A9388|7ec28a9388|4|c4653601|1|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|2735388
+Var #7EC28A938C|7ec28a938c|4|c4653601|1|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|273538c
+Var #7EC28A9390|7ec28a9390|4|c4653601|1|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|2735390
+Var #7EC28A9394|7ec28a9394|4|c4653601|1|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|2735394
+Var #7EC28A9398|7ec28a9398|4|c4653601|1|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|2735398
+Var #7EC28A939C|7ec28a939c|4|c4653601|1|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|273539c
+Var #7EC28A93A0|7ec28a93a0|4|c4653601|1|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|27353a0
+Var #7EC28A93A4|7ec28a93a4|4|c4653601|1|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|27353a4
+Var #7EC28A93A8|7ec28a93a8|4|c4653601|1|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|27353a8
+Var #7EC28A93AC|7ec28a93ac|4|c4653601|1|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|27353ac
+Var #7EC28A93B0|7ec28a93b0|4|c4653601|1|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|27353b0
+Var #7EC28A93B4|7ec28a93b4|4|c4653601|1|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|27353b4
+Var #7EC28A93B8|7ec28a93b8|4|c4653601|1|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|27353b8
+Var #7EC28A93BC|7ec28a93bc|4|c4653601|1|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|27353bc
+Var #7EC28A93C0|7ec28a93c0|4|c4653601|1|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|27353c0
+Var #7EC28A93C4|7ec28a93c4|4|c4653601|1|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|27353c4
+Var #7EC28A93C8|7ec28a93c8|4|c4653601|1|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|27353c8
+Var #7EC28A93CC|7ec28a93cc|4|c4653601|1|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|27353cc
+
+
+]]):close()
+  gg.sleep(50)
+  gg.clearList()
+  gg.clearResults()
+  gg.toast("NO")
+end
+
+function AZH()
+  gg.clearResults()
+  io.output(fileData):write([[
+32020
+Var #76CF125DC0|76cf125dc0|10|3e99999a|0|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|24cadc0
+Var #7EAF390380|7eaf390380|4|100000000|0|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|2735380
+Var #7EAF390384|7eaf390384|4|200000001|0|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|2735384
+Var #7EAF390388|7eaf390388|4|300000002|0|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|2735388
+Var #7EAF39038C|7eaf39038c|4|400000003|0|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|273538c
+Var #7EAF390390|7eaf390390|4|500000004|0|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|2735390
+Var #7EAF390394|7eaf390394|4|600000005|0|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|2735394
+Var #7EAF390398|7eaf390398|4|700000006|0|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|2735398
+Var #7EAF39039C|7eaf39039c|4|800000007|0|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|273539c
+Var #7EAF3903A0|7eaf3903a0|4|900000008|0|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|27353a0
+Var #7EAF3903A4|7eaf3903a4|4|1000000009|0|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|27353a4
+Var #7EAF3903A8|7eaf3903a8|4|1100000010|0|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|27353a8
+Var #7EAF3903AC|7eaf3903ac|4|1200000011|0|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|27353ac
+Var #7EAF3903B0|7eaf3903b0|4|1300000012|0|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|27353b0
+Var #7EAF3903B4|7eaf3903b4|4|1400000013|0|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|27353b4
+Var #7EAF3903B8|7eaf3903b8|4|1500000014|0|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|27353b8
+Var #7EAF3903BC|7eaf3903bc|4|1600000015|0|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|27353bc
+Var #7EAF3903C0|7eaf3903c0|4|1700000016|0|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|27353c0
+Var #7EAF3903C4|7eaf3903c4|4|1800000017|0|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|27353c4
+Var #7EAF3903C8|7eaf3903c8|4|1900000018|0|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|27353c8
+Var #7EAF3903CC|7eaf3903cc|4|2000000019|0|0|0|0|r-xp|/data/app/com.olzhas.carparking.multyplayer-1/base.apk|27353cc
+
+]]):close()
+  gg.sleep(50)
+  gg.clearList()
+  gg.clearResults()
+  gg.toast("OFF")
+end
+
+function DFHJHGKKKITSS()
+  DFHJHGKKKIT = gg.choice({
+    " 1.NISSAN 350z ",
+    " 2.DODGE VIPER ",
+    " 3.BMW M8 ",
+    " 4.GT 63 ",
+    " 5.BMW i8 ",
+    " 6.LAMBORGHINI URUS ",
+    " 7.TOYOTA CAMRY ",
+    " 8.BMW M5 ",
+    " 9.MERSCEDES BENZ #88 ",
+    " 10.BMW X6 ",
+    " 11.HUMMER H1 ",
+    " 12.NISSAN 200sx ",
+    " 13.INVISIBLE CAR ",
+    " 14.MERSCEDES BENZ 98 ",
+    "🔙 กลับ ",
+    "⛔Exit⛔"
+  }, nil, "UN lock Car add$ All")
+  if DFHJHGKKKIT == nil then
+  else
+  end
+  if DFHJHGKKKIT == 1 then
+    ZA()
+  end
+  if DFHJHGKKKIT == 2 then
+    ZB()
+  end
+  if DFHJHGKKKIT == 3 then
+    ZC()
+  end
+  if DFHJHGKKKIT == 4 then
+    ZD()
+  end
+  if DFHJHGKKKIT == 5 then
+    ZE()
+  end
+  if DFHJHGKKKIT == 6 then
+    ZF()
+  end
+  if DFHJHGKKKIT == 7 then
+    ZG()
+  end
+  if DFHJHGKKKIT == 8 then
+    ZN()
+  end
+  if DFHJHGKKKIT == 9 then
+    ZI()
+  end
+  if DFHJHGKKKIT == 10 then
+    ZL()
+  end
+  if DFHJHGKKKIT == 11 then
+    ZM()
+  end
+  if DFHJHGKKKIT == 12 then
+    ZNN()
+  end
+  if DFHJHGKKKIT == 13 then
+    ZO()
+  end
+  if DFHJHGKKKIT == 14 then
+    ZP()
+  end
+  if DFHJHGKKKIT == 15 then
+    START()
+  end
+  if DFHJHGKKKIT == 16 then
+    Exit()
+  end
+  GHOST = -1
+end
+
+function ZA()
+  gg.setVisible(false)
+  gg.setRanges(gg.REGION_CODE_APP)
+  gg.searchNumber("134", gg.TYPE_DWORD)
+  gg.getResults(39)
+  gg.editAll("325", gg.TYPE_DWORD)
+  gg.clearResults(39)
+  gg.toast(" กด buy")
+end
+
+function ZB()
+  gg.setVisible(false)
+  gg.setRanges(gg.REGION_CODE_APP)
+  gg.searchNumber("134", gg.TYPE_DWORD)
+  gg.getResults(39)
+  gg.editAll("321", gg.TYPE_DWORD)
+  gg.clearResults(39)
+  gg.toast(" กด buy")
+end
+
+function ZC()
+  gg.setVisible(false)
+  gg.setRanges(gg.REGION_CODE_APP)
+  gg.searchNumber("134", gg.TYPE_DWORD)
+  gg.getResults(39)
+  gg.editAll("324", gg.TYPE_DWORD)
+  gg.clearResults(39)
+  gg.toast(" กด buy")
+end
+
+function ZD()
+  gg.setVisible(false)
+  gg.setRanges(gg.REGION_CODE_APP)
+  gg.searchNumber("134", gg.TYPE_DWORD)
+  gg.getResults(39)
+  gg.editAll("322", gg.TYPE_DWORD)
+  gg.clearResults(39)
+  gg.toast(" กด buy")
+end
+
+function ZE()
+  gg.setVisible(false)
+  gg.setRanges(gg.REGION_CODE_APP)
+  gg.searchNumber("134", gg.TYPE_DWORD)
+  gg.getResults(39)
+  gg.editAll("312", gg.TYPE_DWORD)
+  gg.clearResults(39)
+  gg.toast(" กด buy")
+end
+
+function ZF()
+  gg.setVisible(false)
+  gg.setRanges(gg.REGION_CODE_APP)
+  gg.searchNumber("134", gg.TYPE_DWORD)
+  gg.getResults(39)
+  gg.editAll("293", gg.TYPE_DWORD)
+  gg.clearResults(39)
+  gg.toast(" กด buy")
+end
+
+function ZG()
+  gg.setVisible(false)
+  gg.setRanges(gg.REGION_CODE_APP)
+  gg.searchNumber("134", gg.TYPE_DWORD)
+  gg.getResults(39)
+  gg.editAll("288", gg.TYPE_DWORD)
+  gg.clearResults(39)
+  gg.toast(" กด buy")
+end
+
+function ZN()
+  gg.setVisible(false)
+  gg.setRanges(gg.REGION_CODE_APP)
+  gg.searchNumber("134", gg.TYPE_DWORD)
+  gg.getResults(39)
+  gg.editAll("260", gg.TYPE_DWORD)
+  gg.clearResults(39)
+  gg.toast(" กด buy")
+end
+
+function ZI()
+  gg.setVisible(false)
+  gg.setRanges(gg.REGION_CODE_APP)
+  gg.searchNumber("134", gg.TYPE_DWORD)
+  gg.getResults(39)
+  gg.editAll("276", gg.TYPE_DWORD)
+  gg.clearResults(39)
+  gg.toast(" กด buy")
+end
+
+function ZL()
+  gg.setVisible(false)
+  gg.setRanges(gg.REGION_CODE_APP)
+  gg.searchNumber("134", gg.TYPE_DWORD)
+  gg.getResults(39)
+  gg.editAll("280", gg.TYPE_DWORD)
+  gg.clearResults(39)
+  gg.toast(" กด buy")
+end
+
+function ZM()
+  gg.setVisible(false)
+  gg.setRanges(gg.REGION_CODE_APP)
+  gg.searchNumber("134", gg.TYPE_DWORD)
+  gg.getResults(39)
+  gg.editAll("281", gg.TYPE_DWORD)
+  gg.clearResults(39)
+  gg.toast(" กด buy")
+end
+
+function ZNN()
+  gg.setVisible(false)
+  gg.setRanges(gg.REGION_CODE_APP)
+  gg.searchNumber("134", gg.TYPE_DWORD)
+  gg.getResults(39)
+  gg.editAll("305", gg.TYPE_DWORD)
+  gg.clearResults(39)
+  gg.toast(" กด buy")
+end
+
+function ZO()
+  gg.setVisible(false)
+  gg.setRanges(gg.REGION_CODE_APP)
+  gg.searchNumber("134", gg.TYPE_DWORD)
+  gg.getResults(39)
+  gg.editAll("290", gg.TYPE_DWORD)
+  gg.clearResults(39)
+  gg.toast(" กด buy")
+end
+
+function ZP()
+  gg.setVisible(false)
+  gg.setRanges(gg.REGION_CODE_APP)
+  gg.searchNumber("134", gg.TYPE_DWORD)
+  gg.getResults(39)
+  gg.editAll("310", gg.TYPE_DWORD)
+  gg.clearResults(39)
+  gg.toast(" กด buy")
+end
+
+function Exit()
+  gg.toast("Exit")
+  print("  ┌─┐  .─┐")
+  print("  │▒│ /▒/      ")
+  print("  │▒│/▒/       ")
+  print("  │▒/▒/─┬─┐")
+  print("  │▒│▒|▒│▒│ ")
+  print("┌┴─┴─┐-┘─┘  ")
+  print("│▒┌──┘▒▒▒│ ")
+  print("└┐▒▒▒▒▒▒┌┘")
+  print("  └┐▒▒▒▒┌┘")
+  os.exit()
+end
+
+while true do
+  if gg.isVisible(true) then
+    GHOST = 1
+    gg.setVisible(false)
+  end
+  if GHOST == 1 then
+    START()
+  end
+  if GHOST == 2 then
+    START()
+  end
+  GHOST = -1
+end
